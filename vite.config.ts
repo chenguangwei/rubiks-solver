277f8dee3d08fe526b883dcc647e26e09a4f02ec
chenguangwei <chenguangwei1017@163.com>
2026-05-30 18:19:18 +0800

    Fix cubing worker preload error

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: process.env.VITE_BASE_PATH ?? '/',
   plugins: [react()],
+  build: {
+    modulePreload: false,
+  },
   optimizeDeps: {
     exclude: ['cubing'],
   },
